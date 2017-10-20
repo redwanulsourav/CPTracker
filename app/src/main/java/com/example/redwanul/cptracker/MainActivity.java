4747ce28f8f0434973001319879b659aab75a2fd
souravvv <redwanul.sourav@gmail.com>
2017-10-20 09:05:12 +0600

    Created Authentication Check

---
+++ app/src/main/java/com/example/redwanul/cptracker/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.redwanul.cptracker;
+
+import android.content.Intent;
+import android.icu.util.TimeUnit;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            Intent intent = new Intent(this,login_signup.class);
+            startActivity(intent);
+        }
+        else{
+
+        }
+    }
+}
